920d733229c2df038bfecd8412ae088f6c4cfdb7
behraamkhan2004-commits <behraamkhan2004@gmail.com>
2026-06-11 12:23:50 -0700

    Added psycopg2 and fixed database config for PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -64,11 +64,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
 }
 
 AUTH_PASSWORD_VALIDATORS = [
